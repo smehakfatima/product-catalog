b939af52320888a517c1c61f78dc0403ece7b2da
Mehak fatima Sheikh <mohsin@livewirelabs.co>
2019-03-26 04:07:37 +0500

    Create a UI to create new posts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  
+  before_action :redirect_if_not_signed_in, only: [:new]
   def show
     @product = Product.find(params[:id])
   end
@@ -16,7 +16,28 @@ class ProductsController < ApplicationController
     products_for_branch(params[:action])
   end
 
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save 
+      redirect_to product_path(@product) 
+    else
+      redirect_to root_path
+    end
+  end
+
+
   private
+
+  def product_params
+    params.require(:product).permit(:description, :name, :category_id)
+                         .merge(user_id: current_user.id)
+  end
 
 	def products_for_branch(branch)
 	  @categories = Category.where(branch: branch)
