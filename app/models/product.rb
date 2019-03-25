1dde48d7344757df5b60bc95098e7a18a85bb8fc
Mehak fatima Sheikh <mohsin@livewirelabs.co>
2019-03-26 04:15:32 +0500

    Add validations to the Post model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,14 @@ class Product < ApplicationRecord
 	belongs_to :user
   	belongs_to :category
 
+  	validates :name, presence: true, length: { minimum: 5, maximum: 255 }
+	validates :description, presence: true, length: { minimum: 20, maximum: 1000 }
+	validates :category_id, presence: true
+
   	default_scope -> { includes(:user).order(created_at: :desc) }
 
   	scope :by_category, -> (branch, category_name) do 
 	  joins(:category).where("categories.name = ? AND categories.branch = ?",category_name, branch)
-	  # .where(categories: {name: category_name, branch: branch}) 
 	end
 
 	scope :by_branch, -> (branch) do
