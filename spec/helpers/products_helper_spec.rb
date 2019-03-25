4397a1f68ca3b21db27590b8f30ea2f9deac967e
Mehak fatima Sheikh <mohsin@livewirelabs.co>
2019-03-26 02:47:24 +0500

    Add specs for ProductsHelper methods

--- spec/helpers/products_helper_spec.rb
+++ spec/helpers/products_helper_spec.rb
@@ -1,15 +1,52 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ProductsHelper. For example:
-#
-# describe ProductsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ProductsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context '#create_new_product_partial_path' do
+    it "returns a signed_in partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      expect(helper.create_new_product_partial_path). to (
+        eq 'products/branch/create_new_product/signed_in'
+      )
+    end
+
+    it "returns a signed_in partial's path" do
+      helper.stub(:user_signed_in?).and_return(false)
+      expect(helper.create_new_product_partial_path). to (
+        eq 'products/branch/create_new_product/not_signed_in'
+      )
+    end
+  end
+
+  context '#all_categories_button_partial_path' do
+    it "returns an all_selected partial's path" do
+      controller.params[:category] = ''
+      expect(helper.all_categories_button_partial_path).to (
+        eq 'products/branch/categories/all_selected'
+      )
+    end
+
+    it "returns an all_not_selected partial's path" do
+      controller.params[:category] = 'category'
+      expect(helper.all_categories_button_partial_path).to (
+        eq 'products/branch/categories/all_not_selected'
+      )
+    end
+  end
+
+  context '#no_products_partial_path' do
+    it "returns a no_products partial's path" do
+      assign(:products, [])
+      expect(helper.no_products_partial_path).to (
+        eq 'products/branch/no_products'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      assign(:products, [1])
+      expect(helper.no_products_partial_path).to (
+        eq 'shared/empty_partial'
+      )
+    end
+  end
 end
